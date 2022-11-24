e2ea46214a640d1e7e7d5cd3b6ff70520700a529
dimkabrz <lvbnhbq1969741>
2022-11-24 17:25:40 +0300

    create context add delete note feature create fakejson style navBar, mainContainer, sideBar, workSpace

--- src/components/UI/Sidebar/SideBar.jsx
+++ src/components/UI/Sidebar/SideBar.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ListItem from "../../ListItem/ListItem";
+import React, {useContext} from 'react';
 import classes from "./SideBar.module.css";
+import NoteCard from "../../NoteCard/NoteCard";
+import {Context} from '../../Context'
+
+
+
 const SideBar = () => {
+    const {notes} = useContext(Context)
 
 
     return (
-        <div className={classes.sideBar}>
-            <ListItem/>
-            <ListItem/>
-        </div>
+            <div className={classes.sideBar}>
+                {notes.map(note=>(
+                    <NoteCard note={note} key={note.id}/>
+                ))}
+            </div>
     );
 };
 

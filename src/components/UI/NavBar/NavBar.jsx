b2f4424fdb44ed5d193640dae85fbfda0b1fa367
dimkabrz <dima_b95@mail.ru>
2022-11-24 22:00:02 +0300

    add search note feature style noteCard, mainContainer

--- src/components/UI/NavBar/NavBar.jsx
+++ src/components/UI/NavBar/NavBar.jsx
@@ -1,46 +1,66 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from "react";
 import classes from "./NavBar.module.css";
-import {Button, Input} from "antd";
+import { Button, Input } from "antd";
 import Modal from "antd/es/modal/Modal";
-import { SearchOutlined } from "@ant-design/icons";
-import {Context} from "../../Context";
+import {DeleteOutlined, EditOutlined, SearchOutlined} from "@ant-design/icons";
+import { Context } from "../../Context";
 import axios from "axios";
 
-
 const NavBar = () => {
+  const { chosenNote, getNotes, setNotes } = useContext(Context);
 
-    const {chosenNote,getNotes} = useContext(Context)
+  const [search, setSearch] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = async () => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3001/notes/${chosenNote.id}`
+      );
+      getNotes();
+      setIsModalOpen(false);
+    } catch {}
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
+  const fetchSetter = async (searchText) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/notes?q=${searchText}`
+      );
+      setNotes(response.data);
+    } catch {}
+  };
 
-    const onSearch = (value) => console.log(value);
-    const [search, setSearch] = useState('');
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
-    const handleOk = async () => {
-        const response = await axios.delete(`http://localhost:3001/notes/${chosenNote.id}`);
-        getNotes();
-        setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <div className={classes.navBar}>
-
-            <Modal title="Удалить заметку?" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-            </Modal>
-            <Button onClick={showModal}>Удалить задачу</Button>
-            <Input
-                className={classes.navBar_input}
-                value={search}
-                // onChange={(e) => getContact(e)}
-                prefix={<SearchOutlined />}
-            />
-        </div>
-    );
+  const getContact = async (e) => {
+    const searchText = e.target.value;
+    setSearch(() => {
+      fetchSetter(searchText);
+      return searchText;
+    });
+  };
+  return (
+    <div className={classes.navBar}>
+      <Modal
+        title="Удалить заметку?"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      ></Modal>
+      <Button><EditOutlined /></Button>
+      <Button onClick={showModal}><DeleteOutlined /></Button>
+      <Input
+        className={classes.navBar_input}
+        value={search}
+        onChange={(e) => getContact(e)}
+        prefix={<SearchOutlined />}
+      />
+    </div>
+  );
 };
 
 export default NavBar;
--- src/components/WorkSpace/WorkSpace.jsx
+++ src/components/WorkSpace/WorkSpace.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import SideBar from "../UI/Sidebar/SideBar";
 import MainContainer from "../MainCotainer/MainContainer";
 import classes from "./WorkSpace.module.css";
 
 const WorkSpace = () => {
-    return (
-        <div className={classes.workSpace}>
-            <SideBar/>
-            <MainContainer/>
-        </div>
-    );
+  return (
+    <div className={classes.workSpace}>
+      <SideBar />
+      <MainContainer />
+    </div>
+  );
 };
 
 export default WorkSpace;
